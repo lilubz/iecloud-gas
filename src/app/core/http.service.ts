c56ea672db14a6182667ffb314d62158e3dbdff3
aiyuezhicheng <15545138785@163.com>
2017-10-24 10:10:00 +0800

    Wenwu (#3)
    * 气瓶信息精确查询
    
    * 使用本地classList.min.js文件

--- src/app/core/http.service.ts
+++ src/app/core/http.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { Headers, Http, ResponseContentType } from '@angular/http';
 
--- src/app/supervise/cylinder/cylinder-detail/cylinder-detail.component.ts
+++ src/app/supervise/cylinder/cylinder-detail/cylinder-detail.component.ts
@@ -1,14 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { detailList } from './detail';
+import { CylinderDetailService } from './cylinder-detail.service';
 @Component({
   selector: 'gas-cylinder-detail',
   templateUrl: './cylinder-detail.component.html',
-  styleUrls: ['./cylinder-detail.component.css']
+  styleUrls: ['./cylinder-detail.component.scss'],
+  providers: [CylinderDetailService]
 })
 
 export class CylinderDetailComponent implements OnInit {
+  // searchParams: {
+  //   cylinderCode: string;
+  // };
+  detailList: Array<{
+    borough: string;
+    enterpriseName: string;
+    serviceCondition: string;
+    fillingState: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
+    // borough: string;
 
-  constructor() { }
-
-  ngOnInit() { }
+  }>;
+  photoList: Array<{
+    pictureUrl: string;
+  }>;
+  // constructor(private CylinderDetailService: CylinderDetailService) { }
+  getList() {
+    this.detailList = detailList;
+    console.log(detailList);
+  //   const params = {
+  //     cylinderCode: this.searchParams.cylinderCode,
+  //   };
+  //   this.CylinderDetailService.querySingle(params).then(data => {
+  //     this.detailList = data.data.list;
+  //     this.photoList = data.data.pictureList;
+  //   });
+  }
+  ngOnInit() {
+    this.getList();
+  }
 }
