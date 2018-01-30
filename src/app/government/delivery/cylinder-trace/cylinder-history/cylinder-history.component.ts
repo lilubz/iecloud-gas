b939716ac521840cccd951ab227515f1a2270257
voi4 <2280052873@qq.com>
2018-01-30 22:45:35 +0800

    隐藏视频监控下载功能
    Squash from b53af85 to d9c6c0d by voi4

--- src/app/government/delivery/cylinder-trace/cylinder-history/cylinder-history.component.ts
+++ src/app/government/delivery/cylinder-trace/cylinder-history/cylinder-history.component.ts
@@ -84,11 +84,13 @@ export class CylinderHistoryComponent implements OnInit {
         this.cylinderHistoryList = data.data;
         this.totalHistory = data.data.length;
       } else {
+        this.loading = false;
         this.cylinderHistoryList = [];
         this.totalHistory = 0;
         this.messageService.add({ severity: 'warn', summary: '获取气瓶状态历史失败', detail: data.msg });
       }
     }).catch(error => {
+      this.loading = false;
       this.cylinderHistoryList = [];
       this.totalHistory = 0;
       this.messageService.add({ severity: 'warn', summary: '获取气瓶状态历史失败', detail: error });
