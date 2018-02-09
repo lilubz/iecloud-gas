ec3788edf3b94aa3269db2136046fe1b89f161a6
voi4 <2280052873@qq.com>
2018-02-09 20:33:49 +0800

    修改[政府]用户综合查询+车辆信息+送气工信息的样式
    Squash from d968cec to 67bdd7b by voi4

--- proxy.conf.js
+++ proxy.conf.js
@@ -31,6 +31,16 @@ const PROXY_CONFIG = [
   //     "^/wenZhouGas": ""
   //   }
   // },
+  {
+    context: [
+      "/wenZhouGas",
+    ],
+    target: "http://192.168.1.8:8080", // 妮娜
+    secure: false,
+    "pathRewrite": {
+      "^/wenZhouGas": ""
+    }
+  },
   // {
   //   context: [
   //     "/wenZhouGas",
