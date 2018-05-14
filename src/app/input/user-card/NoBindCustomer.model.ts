511383e9b92a19929808005babfa2dd9a5dabddc
Hu Zhibin <gyzyww@gmail.com>
2018-05-14 14:43:53 +0800

    修复身份认证卡绑定的bug

---
+++ src/app/input/user-card/NoBindCustomer.model.ts
@@ -0,0 +1,8 @@
+export interface NoBindCustomer {
+  userNumber: number,
+  userName: string,
+  certificateName: string,
+  deliveryAddress: string,
+  certificateId: string,
+  userStatus?: string,
+}
--- src/app/input/user-card/user-card.component.ts
+++ src/app/input/user-card/user-card.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/components/common/selectitem';
 import { CustomerCard } from './CustomerCard.model';
 import { Customer } from './../user-info/Customer.model';
+import { NoBindCustomer } from './NoBindCustomer.model';
 @Component({
   selector: 'gas-user-card',
   templateUrl: './user-card.component.html',
@@ -23,17 +24,6 @@ export class UserCardComponent implements OnInit {
   ];
   gasSites: SelectItem[]; // 绑定卡时的供气站点下拉列表
 
-  // 绑定用户卡模态框数据
-  bindCustomerToCard: {
-    site: string;
-    userNumber: string;
-    userIdentityCardNumber: string;
-  } = {
-      userIdentityCardNumber: '',
-      userNumber: '',
-      site: '',
-    };
-
   // 申请用户卡模态框数据
   applyCardParams: {
     amount: string;
@@ -46,31 +36,17 @@ export class UserCardComponent implements OnInit {
   customerCards: CustomerCard[] = []; // 申请用户卡列表
 
   // 用户卡绑定--客户搜索建议列表
-  customerSuggestions: {
-    userNumber: string,
-    userName: string,
-    certificateName: string,
-    deliveryAddress: string,
-    certificateId: string,
-    userStatus: string,
-  }[] = [];
+  customerSuggestions: NoBindCustomer[] = [];
 
   // 用户卡绑定--选中的用户
-  selectedCustomer: {
-    userNumber?: string,
-    userName?: string,
-    certificateName?: string,
-    deliveryAddress?: string,
-    certificateId?: string,
-    userStatus?: string,
-  } = {
-      userNumber: '',
-      userName: '',
-      certificateName: '',
-      deliveryAddress: '',
-      certificateId: '',
-      userStatus: ''
-    };
+  selectedCustomer: NoBindCustomer = {
+    userNumber: null,
+    userName: '',
+    certificateName: '',
+    deliveryAddress: '',
+    certificateId: '',
+    userStatus: ''
+  };
 
   customerCardSuggestions: CustomerCard[] = []; // 用户卡绑定--用户卡搜索建议列表
   selectedCustomerCard: CustomerCard = new CustomerCard(); // 用户卡绑定--选中的用户卡
@@ -104,7 +80,6 @@ export class UserCardComponent implements OnInit {
     });
   }
   searchCustomer(event) {
-    console.log(event);
     this.userCardService.getUnbindCustomerList({
       pageNum: 1,
       pageSize: 20,
@@ -114,34 +89,42 @@ export class UserCardComponent implements OnInit {
       if (data.status === 0) {
         this.customerSuggestions = data.data.list;
       } else {
+        this.customerSuggestions = [];
         this.messageService.add({ severity: 'warn', summary: '', detail: data.msg });
       }
     });
   }
-  selectSuggestedCustomer(event: any) {
-    console.log(event);
-    this.selectedCustomer = event;
-    this.bindCustomerToCard.userNumber = event.userNumber;
+  blurSelectedCustomer() {
+    if (!this.selectedCustomer || !this.selectedCustomer.userNumber) {
+      this.selectedCustomer = {
+        userNumber: null,
+        userName: '',
+        certificateName: '',
+        deliveryAddress: '',
+        certificateId: '',
+        userStatus: ''
+      };
+    }
   }
 
   searchCustomerCard(event) {
-    console.log(event);
     this.userCardService.getUnbindCardList({
       pageNum: 1,
       pageSize: 20,
-      userIdentityCardNumber: this.bindCustomerToCard.userIdentityCardNumber
+      userIdentityCardNumber: event.query
     }).then(data => {
       if (data.status === 0) {
         this.customerCardSuggestions = data.data.list;
       } else {
+        this.customerCardSuggestions = [];
         this.messageService.add({ severity: 'warn', summary: '', detail: data.msg });
       }
     });
   }
-  selectSuggestedCustomerCard(event: any) {
-    console.log(event);
-    this.selectedCustomerCard = event;
-    this.bindCustomerToCard.userIdentityCardNumber = event.userIdentityCardNumber;
+  blurSelectedCustomerCard() {
+    if (!this.selectedCustomerCard || !this.selectedCustomerCard.userIdentityCardNumber) {
+      this.selectedCustomerCard = new CustomerCard();
+    }
   }
 
   applyCard() {
@@ -160,34 +143,30 @@ export class UserCardComponent implements OnInit {
   }
 
   showBindCardDialog(customerCard?: CustomerCard) {
-    console.log(customerCard);
     this.bindCardVisible = true;
     this.selectedCustomer = {
-      userNumber: '',
+      userNumber: null,
       userName: '',
       certificateName: '',
       deliveryAddress: '',
       certificateId: '',
       userStatus: ''
     };
-    this.bindCustomerToCard.userNumber = '';
     this.selectedCustomerCard = customerCard || new CustomerCard();
-    this.bindCustomerToCard.userIdentityCardNumber = customerCard ? customerCard.userIdentityCardNumber : '';
   }
 
   bind() {
-    if (this.bindCustomerToCard.userNumber === '') {
+    if (!this.selectedCustomer.userNumber) {
       this.messageService.add({ severity: 'warn', summary: '', detail: '请输入绑定用户的名称或证件编号！' });
       return;
-    } else if (this.bindCustomerToCard.userIdentityCardNumber === '') {
+    } else if (!this.selectedCustomerCard.userIdentityCardNumber) {
       this.messageService.add({ severity: 'warn', summary: '', detail: '请输入身份认证卡编号！' });
       return;
     }
 
     this.userCardService.bindCustomerAndCard({
-      userNumber: this.bindCustomerToCard.userNumber,
-      userIdentityCardNumber: this.bindCustomerToCard.userIdentityCardNumber,
-      // site: this.bindCustomerToCard.site
+      userNumber: this.selectedCustomer.userNumber,
+      userIdentityCardNumber: this.selectedCustomerCard.userIdentityCardNumber
     }).then(data => {
       if (data.status === 0) {
         this.messageService.add({ severity: 'success', summary: '绑定成功', detail: data.msg });
