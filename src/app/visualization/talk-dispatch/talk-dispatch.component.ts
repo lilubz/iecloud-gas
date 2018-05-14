0fb399f6484f61befcab3774b9e35437d0bb6cc6
Hu Zhibin <gyzyww@gmail.com>
2018-05-14 20:53:12 +0800

    指挥调度添加浏览器检测

--- src/app/visualization/talk-dispatch/talk-dispatch.component.ts
+++ src/app/visualization/talk-dispatch/talk-dispatch.component.ts
@@ -10,4 +10,14 @@ export class TalkDispatchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  // 浏览器检查
+  isIE(): boolean {
+    const ua = window.navigator.userAgent;
+    let version = 0;
+    if (/MSIE([^;]+)/.test(ua)) { // 判断IE11以下（不含IE11）
+      version = parseFloat(RegExp['$1']);
+    }
+    return version > 0;
+  }
 }
