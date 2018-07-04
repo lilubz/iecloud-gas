de05463897351c3810d708e2437cc8e7c57e09c3
lihao1024 <1914204500@qq.com>
2018-07-04 20:16:58 +0800

    减少层级

--- proxy.conf.js
+++ proxy.conf.js
@@ -20,16 +20,16 @@ const PROXY_CONFIG = [
   //   //   "^/wenZhouGas": ""
   //   // }
   // },
-  {
-    context: [
-      "/wenZhouGas",
-    ],
-    target: "http://192.168.1.8:8080", // 妮娜
-    secure: false,
-    "pathRewrite": {
-      "^/wenZhouGas": ""
-    }
-  },
+  // {
+  //   context: [
+  //     "/wenZhouGas",
+  //   ],
+  //   target: "http://192.168.1.8:8080", // 妮娜
+  //   secure: false,
+  //   "pathRewrite": {
+  //     "^/wenZhouGas": ""
+  //   }
+  // },
   // {
   //   context: [
   //     "/wenZhouGas",
@@ -52,17 +52,17 @@ const PROXY_CONFIG = [
   //     "^/wenZhouGas": ""
   //   }
   // },
-  // {
-  //   context: [
-  //     "/wenZhouGas",
-  //     "/dataImport",
-  //   ],
-  //   target: "http://218.75.127.46:18080",// 测试服务器
-  //   secure: false,
-  //   // "pathRewrite": {
-  //   //   "^/wenZhouGas": ""
-  //   // }
-  // },
+  {
+    context: [
+      "/wenZhouGas",
+      "/dataImport",
+    ],
+    target: "http://218.75.127.46:18080",// 测试服务器
+    secure: false,
+    // "pathRewrite": {
+    //   "^/wenZhouGas": ""
+    // }
+  },
   // {
   //   context: [
   //     "/wenZhouGas",
--- src/app/system/architecture-management/architecture-management.module.ts
+++ src/app/system/architecture-management/architecture-management.module.ts
@@ -1,10 +1,8 @@
 import { NgModule, } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
-import { MangeMentComponent } from './mange-ment/mange-ment.component';
 
 @NgModule({
   declarations: [
-    MangeMentComponent
   ],
   imports: [
     SharedModule,
