9e2b62bc0ab8e5b97378743de16ba9f90c0717e3
lihao1024 <1914204500@qq.com>
2018-07-05 13:40:46 +0800

    文件上传兼容问题

--- src/app/shared/success/success.component.ts
+++ src/app/shared/success/success.component.ts
@@ -15,18 +15,18 @@ export class SuccessComponent implements OnInit,OnDestroy {
 
   ngOnInit() {
     let oldUrl=this.route.queryParams['value'].url;
-    
+
     this.returnURL = oldUrl || '';
     // console.log(this.returnURL);
-    
+
     this.setIntervalUrl = setInterval(go, 1000);
-    var second = 3; //利用了全局变量来执行  
+    var second = 3; //利用了全局变量来执行
     function go() {
       second--;
       if (second > 0) {
-        document.getElementById("totalSecond").innerHTML = second + ''; //每次设置的second的值都不一样了。  
+        document.getElementById("totalSecond").innerHTML = second + ''; //每次设置的second的值都不一样了。
       } else {
-        location.href = ('#'+oldUrl) || '';
+        location.href = ('#' + oldUrl) || '';
       }
     }
   }
